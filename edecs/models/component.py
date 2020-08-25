0bdc3ebd3694fc256ef3acfd082fab9ed9ae9bf6
Laminariy <dovakin121212@gmail.com>
2020-08-25 23:38:22 +0300

    Component base

--- edecs/models/component.py
+++ edecs/models/component.py
@@ -1,4 +1,57 @@
 class Component():
+    '''Base component class'''
 
-    def __init__(self):
+    #__slots__ = ['_type', '_entity']
+
+    default_component_type = None
+    defaults = {}
+
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def entity(self):
+        return self._entity
+
+    @property
+    def initialized(self):
+        return self._entity
+
+    # TO DO: look __new__ function in tutorial (append __slots__)
+
+    def __init__(self, component_type=None, **properties):
+        self._type = component_type or self.default_component_type or \
+                                                            type(self).__name__
+        self._entity = None
+
+        if self.defaults == {}:
+            for key, value in properties.items():
+                setattr(self, key, value)
+        else:
+            for key, value in self.defaults.items():
+                setattr(self, key, properties.pop(key, value))
+
+    def __repr__(self):
+        # <Component:type; Enity:entity>
+        return "<Component:{}; Entity:{}>".format(self._type,
+                                            self._entity if self._entity is None
+                                                        else self._entity.type)
+
+    '''def __str__(self):
+        # JSON
+        pass'''
+
+
+    def create(self, entity):
+        if self.initialized then:
+            # TO DO: raise error (component already exist(have entity))
+            pass
+
+        setattr(entity, self._type, self)
+
+    def destroy(self):
+        # TO DO: destroy component
+        # TO DO: raise error if component is not exist (no entity)
         pass

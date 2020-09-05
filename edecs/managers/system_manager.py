7fe2ad5af06c8ca53fecdb1b79082bc2769181b2
Laminariy <dovakin121212@gmail.com>
2020-09-06 01:27:31 +0300

    System manager

--- edecs/managers/system_manager.py
+++ edecs/managers/system_manager.py
@@ -1,4 +1,24 @@
 class SystemManager():
+    '''System manager'''
+
+    @property
+    def system_types(self):
+        return self._system_types
+
 
     def __init__(self):
-        pass
+        self._system_types = {}
+
+    def create_system(self, system):
+        if system.type in self._system_types.keys():
+            pass
+            # TO DO: Raise error (system already exist)
+
+        self._system_types[system.type] = system
+
+    def destroy_system(self, system):
+        if system.type not in self._system_types.keys():
+            pass
+            # TO DO: Raise error (system already destroyed (doesn't exist))
+
+        del self._system_types[system.type]

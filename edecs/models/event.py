8f658bb2b93a224c8797f79da75e145bbad29557
Laminariy <dovakin121212@gmail.com>
2020-09-07 19:27:43 +0300

    Event representation and string

--- edecs/models/event.py
+++ edecs/models/event.py
@@ -13,3 +13,12 @@ class Event():
     def __init__(self, type, data):
         self._type = type
         self._data = data
+
+    def __repr__(self):
+        # <Event:event_type>
+        return "<Event:{}".format(self.type)
+
+    def __str__(self):
+        # <Event:event_type>
+        # {data}
+        return "<Event:{}>\n{}".format(self.type, self.data)

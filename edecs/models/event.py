aeb66dc6311b811fb2f6b31df2928cc0eec3f8f1
Laminariy <dovakin121212@gmail.com>
2020-09-07 17:39:37 +0300

    Event class

--- edecs/models/event.py
+++ edecs/models/event.py
@@ -1,4 +1,15 @@
 class Event():
+    '''Event class'''
 
-    def __init__(self):
-        pass
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def data(self):
+        return self._data
+
+
+    def __init__(self, type, data):
+        self._type = type
+        self._data = data

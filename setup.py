9c2ac6096462cf12a8bfabc484c8e8bf8d0c31cd
Laminariy <dovakin121212@gmail.com>
2020-11-22 17:03:54 +0300

    0.1.18

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='edecs',
-      version='0.1.17',
+      version='0.1.18',
       description='Event-driven Entity-Component-System engine',
       packages=find_packages(),
       author_email='dovakin121212@gmail.com',

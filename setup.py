0b93695128a73bd52dbe720927f122e2a038e12a
Stanislav Serebryakov <dovakin121212@gmail.com>
2020-11-25 23:18:20 +0300

    0.2.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='edecs',
-      version='0.2.0',
+      version='0.2.1',
       description='Event-driven Entity-Component-System engine',
       packages=find_packages(),
       author_email='dovakin121212@gmail.com',
